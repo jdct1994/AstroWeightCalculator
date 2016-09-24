2b59bd9ea8a6604e8a03f4fa930e54aa585c216e
John-Divino Collado Talamayan Jr <Talamayan1@John-Divinos-MacBook-Air.local>
2016-09-23 17:47:19 -0700

    Project 1 Draft 2

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-/* First define what we need to do. We need to loop through each of the Planets with Javascript
-or something like that... 
-
+/* First define what we need to do. We need to loop through each of the Planets with Javascript,
 Generate the display title of the Astro Weight Calculator by reversing the string, 
 'rotaluclaC thgieW ortsA', using JavaScript and binding the result to a span tag 
 using getElementById() syntax.
 
-Calculate the weight by multiplying input by gravity
+Calculate the weight by multiplying input by gravity.
 
-Populate the select element options dynamically using this array of objects:
-
+Populate the select element options dynamically using the array of objects var planets:
 */
 
 var planets = [
@@ -27,48 +24,38 @@ var planets = [
 
 var button = document.getElementsByTagName("button");
 
-/* Retrieve inputted weights, retrieve gravity multiplier, round 
+/* Retrieve inputted weights, retrieve gravity multiplier, multiply and round 
     to the nearest decimal
-
  */
 
 function calculator()
-{    
+{  
      var select = document.getElementById('planets');
- 
      var options = select.options;
- 
      var selectedOption = options[sel.selectedIndex];
- 
      var planetName = selectedOption.innerHTML;
- 
      var planetWeight = select.value;
-     
      var userWeight = document.getElementById("weight").value;
-        
      var totalWeight = userWeight * planetWeight;
-        
      var roundTotalWeight = Math.round(totalWeight);
-
      var output = document.getElementById('results');
-        
-     output.innerHTML="If you were on "+planetName+ " " + " " +"you would weight"+ " " +roundTotalWeight+ " " + "lbs!";
+
+
+    // Print calculated weight results to the front-end by using if or else statement depending on the planet through .innerHTML
+      if (planetName === 'Sun' || planetName === 'Moon') {
+        document.getElementById("results").innerHTML = "If you were on the " + planetName + " you would weigh " + roundTotalWeight + " lbs.";
+    } else {
+        document.getElementById("results").innerHTML = "If you were on " + planetName + " you would weigh " + roundTotalWeight + " lbs.";
+    }
 }
 
-/*
-san_diego2010 [11:13 AM]  
-function reverseString(str) {
-     return str.split('').reverse().join('');
-}
-     var astro = reverseString('Astro Weight Calculator');
-     document.getElementById("reverse").innerHTML = astro;
+/* Alternate functional coding path for calculator
 
-*/
-
-/*
 function calculator () {
+
     // Retrieves inputted weight from Id of weight
     var weight = document.getElementById("weight").value;
+
     // Retrieves the gravity multiplier
     var gravity = document.getElementById("gravity").value;
 
@@ -77,35 +64,36 @@ function calculator () {
     //Rounds to the nearest decimal
     calculation = calculation.toFixed(0)
 
-    // Print calculated weight results to the front-end by using if or else statement depending on the planet
-    var planetName = document.getElementById("planets")[document.getElementById("planets").selectedIndex].text;
-    if (planetName == 'sun' || planetName == 'moon') {
-        document.getElementById("results").innerHTML = "If you were on the " + planetName + " you would weigh " + calculation + " lbs.";
-    } else {
-        document.getElementById("results").innerHTML = "If you were on " + planetName + " you would weigh " + calculation + " lbs.";
-
-    }
-}
-
 */
 
 
+// Populate the dropdown select menu using a for loop in JavaScript
 var sel = document.getElementById('planets');
 for(var i = 0; i < planets.length; i++) {
     var opt = document.createElement('option');
     opt.value = planets[i].gravity;
     opt.innerHTML = planets[i].planet;
-    // Append the select to an element in the dom
     sel.appendChild(opt);
 }
 
+
+// Reverse string using a loop
 var myTitle = "rotaluclaC thgieW ortsA";
 document.getElementById("displayTitle").innerHTML= reverse(myTitle);
 function reverse(stringToReverse) {
   var rev="";
   for(var i = stringToReverse.length -1; i >= 0; i--){
-
 rev += stringToReverse[i];
   }
   return rev;
 }
+
+/* Alternate functional coding path for reversing string.
+
+function reverseString(str) {
+     return str.split('').reverse().join('');
+}
+     var astro = reverseString('Astro Weight Calculator');
+     document.getElementById("displayTitle").innerHTML = astro;
+
+*/
